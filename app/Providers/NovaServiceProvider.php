3a188ab655027b4132e28f9e77a4728b6ba20eed
Matt <matthewjumpsoffbuildings@gmail.com>
2024-09-26 13:54:51 +0930

    init

---
+++ app/Providers/NovaServiceProvider.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Providers;
+
+use App\Nova\Dashboards\Main;
+use App\Nova\Filters\UserFilter;
+use App\Nova\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
+use Laravel\Nova\Nova;
+use Laravel\Nova\NovaApplicationServiceProvider;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('chart-bar'),
+
+                MenuSection::make('Users', [
+                    MenuItem::resource(User::class),
+                    MenuItem::resource(User::class)->filter('Filter 1', User::class, UserFilter::make(), 1),
+                    MenuItem::resource(User::class)->filter('Filter 2', User::class, UserFilter::make(), 2),
+                ])->icon('user')->collapsable(),
+            ];
+        });
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+                ->withAuthenticationRoutes(default: true)
+                ->withPasswordResetRoutes()
+                ->register();
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+    }
+
+    /**
+     * Get the dashboards that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [
+            new \App\Nova\Dashboards\Main,
+        ];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User 1',
+            'email' => 'test1@example.com',
+            'password' => 'test',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'test2@example.com',
+            'password' => 'test',
+        ]);
+    }
+}
